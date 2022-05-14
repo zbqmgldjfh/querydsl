1f5e8cdb7cc98735a2c22354f116a7a03978cf0f
jiwoo <zbqmgldjfh@gmail.com>
2022-05-14 11:49:25 +0900

    test(QueryDslBasicTest): Join의 ON절 공부

--- src/test/java/study/querydsl/QueryDslBasicTest.java
+++ src/test/java/study/querydsl/QueryDslBasicTest.java
@@ -254,4 +254,44 @@ public class QueryDslBasicTest {
         assertThat(result).extracting("username")
                 .containsExactly("teamA", "teamB");
     }
+
+    /**
+     * 회원과 팀을 조인하되, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+     * select m, t from Member m join m.team t on t.name = 'teamA'
+     */
+    @Test
+    public void join_on_filtering_test() {
+        List<Tuple> tuples = queryFactory
+                .select(member, team)
+                .from(member)
+                .join(member.team, team)
+                //.on(team.name.eq("teamA"))
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        for (Tuple tuple : tuples) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    /**
+     * 연관관계가 없는 엔티티 끼리 외부 조인
+     * 회원의 이름이 팀 이름과 같은 대상을 outer join
+     */
+    @Test
+    public void join_on_no_relation() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team).on(member.username.eq(team.name))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
 }
